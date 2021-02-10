b43b338861da4a77ee05ad619ee0fba0627dc026
amberamigo <himanshu180599@gmail.com>
2021-02-10 17:44:53 +0530

    included await method and status codes

--- app.js
+++ app.js
@@ -44,21 +44,19 @@ const Meme = mongoose.model("Meme", memeSchema);
 
 
 
-var editMeme;
+var editMeme;  // id of the meme to be edited
 
-///////////////////////////  home page  //////////////////////
-app.get("/", function(req, res){
-	Meme.find({}, function(err, memes) {
-			res.render("memes", {memeItems : memes});
-	});
+///////////////////////////////////////////  home page  ///////////////////////////////////////////////////
+app.get("/", async function(req, res){
+	const allMemes = await Meme.find({});
+			res.render("memes", {memeItems : allMemes});
 });
 
 
-//////////////////////////////////////////  for memes in json for api //////////////////////////////////////////////////////
-app.get("/memes", function(req, res) {
-	Meme.find({}, function(err, memes) {
-		res.json(memes);
-	});
+//////////////////////////////////////////  to get all memes in JSON //////////////////////////////////////////////////////
+app.get("/memes", async function(req, res) {
+		const allMemes = await Meme.find({});
+		res.json(allMemes);
 });
 
 
@@ -72,21 +70,18 @@ app.post("/", function(req, res) {
 	caption : req.body.caption,
 	url : req.body.url
 	});
-    
-    Meme.find({name : req.body.name, caption : req.body.caption, url : req.body.url}, function(err, memes) {
-			if(memes.length){
+
+	 Meme.find({name : req.body.name, caption : req.body.caption, url : req.body.url})
+	      .then(async function(memes){
+	      	if(memes.length){
 				console.log("exists");
 				res.status(409).send("Oh uh, Duplicate post!!!");
-			}else{
-				uniqueId++;
-	
-                 newMeme.save(function(err){
-    	          if(err)
-    		         console.log(err);
-                 });
-                 res.redirect("/");
-			}
-	});
+			   }else{
+			   	   uniqueId++;
+                   const savedMeme = await newMeme.save();
+			   }
+			    res.redirect("/");
+			});
 });
 /////////////////////////////////////////////////// for post using api calls /////////////////////////////////////////////
 app.post("/memes", function(req, res) {
@@ -116,41 +111,76 @@ app.post("/memes", function(req, res) {
 
 
 ///////////////////////////////////////////// API call for a particular meme ///////////////////////////////////////////
-app.get("/memes/:memeId", function(req, res) {
+app.get("/memes/:memeId", async function(req, res) {
 	const id = req.params.memeId;
-	console.log(id);
-	Meme.find({id : id}, function(err, meme) {
+  try{
+	await Meme.find({id : id}, function(err, meme) {
 		if(!meme.length){
 			res.status(404).send("Oh uh, No match found");
 		}else{
 			res.json(meme);
 		}
 	});
-    
+    } catch(err){
+    	res.status(400).send({message : err.message});
+    }
 });
 ////////////////////////////////////////////////  updating url and caption  /////////////////////////////////////
-app.patch('/memes/:id', function (req, res) {
-	var memeId = req.params.id;
-     Meme.updateOne({id : memeId}, { $set: {caption: req.body.caption, url: req.body.url }}, function(err, res){
-   	if(err)
-   		console.log(err);
-   });
-    res.redirect("/");
+app.patch('/memes/:id', async function (req, res) {
+	cap = req.body.caption;
+	url = req.body.url;
+	editMeme = req.params.id;
+try{
+   if(!cap.length){
+        await Meme.updateOne({id : editMeme}, { $set: {url: req.body.url }}, function(err, res){
+   	        if(err)
+   		        console.log(err);
+            });
+     }else if(!url.length){
+     	await  Meme.updateOne({id : editMeme}, { $set: {caption: req.body.caption}}, function(err, res){
+   	        if(err)
+   		        console.log(err);
+            });
+     }else{
+     	await Meme.updateOne({id : editMeme}, { $set: {caption: req.body.caption, url: req.body.url }}, function(err, res){
+   	        if(err)
+   		        console.log(err);
+            });
+     }
 
+    console.log(res);
+    res.json(res.statusCode);
+} catch(err){
+	res.status(400).json({message : err.message});
+}
 });
 
-app.post('/memes/edit', function (req, res) {
+app.post('/memes/edit', async function (req, res) {
 	cap = req.body.caption;
 	url = req.body.url;
 
-   Meme.find({id : editMeme}, function(err, mem){
-   	console.log(mem);
-   });
-   Meme.updateOne({id : editMeme}, { $set: {caption: req.body.caption, url: req.body.url }}, function(err, res){
-   	if(err)
-   		console.log(err);
-   });
+ try{
+   if(!cap.length){
+        await Meme.updateOne({id : editMeme}, { $set: {url: req.body.url }}, function(err, res){
+   	        if(err)
+   		        console.log(err);
+            });
+     }else if(!url.length){
+     	await  Meme.updateOne({id : editMeme}, { $set: {caption: req.body.caption}}, function(err, res){
+   	        if(err)
+   		        console.log(err);
+            });
+     }else{
+     	await Meme.updateOne({id : editMeme}, { $set: {caption: req.body.caption, url: req.body.url }}, function(err, res){
+   	        if(err)
+   		        console.log(err);
+            });
+     }
+
     res.redirect("/");
+} catch(err){
+	res.status(400).json({message : err.message});
+}
 });
 
 app.post("/edit", function(req, res) {
@@ -159,11 +189,6 @@ app.post("/edit", function(req, res) {
      
     res.render("update");
 });
-
-// function closeForm() {
-//   document.getElementById("myForm").style.display = "none";
-// }
-// })
 
 
 //////////////////////////////////////////////////    server local host //////////////////////////////////////////////
